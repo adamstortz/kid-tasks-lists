ae0525b464242662d2effe6f9da47928c881dd5b
adamstortz <astortz@redstonecontentsolutions.com>
2020-08-13 15:56:18 -0500

    1 file modified - Maya's first commit to source control... ever!!

--- index.js
+++ index.js
@@ -5,12 +5,28 @@ import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 let App = props => {
-  const kids = ["Hannah"];
+  const kids = ["Hannah", "Maya"];
   const days = [{ name: "Sunday" }, { name: "Monday", isSchoolDay: false }];
   const sections = [
-    { name: "Chores", tasks: [{ name: "a" }] },
-    { name: "Dog", tasks: [{ name: "b" }] },
-    { name: "School", tasks: [{ name: "foo", type: "track" }] }
+    { 
+      name: "Chores", 
+      tasks: [
+        { name: "walk" }
+      ] 
+    },
+    { 
+      name: "Dog", 
+      tasks: [
+        { name: "Train" }
+      ] 
+    },
+    { 
+      name: "School", 
+      tasks: [
+        { name: "foos" },
+        { name: "food", type: "track" }
+      ] 
+    }
   ];
   return kids.map(kid => (
     <CheckList key={kid} name={kid} days={days} sections={sections} />
